feecbff23643a9bc165b8c5b6d4bc6caafa87f09
Ayush Shukla <ashukla2705@gmail.com>
2018-06-20 18:49:54 -0400

    Angular link updated in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
+    "http://angular-course-management.herokuapp.com");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
